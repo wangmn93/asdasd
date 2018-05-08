dc9499384613614bce452a6381fd201399ff3f4b
mengnan wang <wangmn93@gmail.com>
2018-05-08 21:41:53 +0200

    first commit

---
+++ cat-gan-lab.py
@@ -0,0 +1,220 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# import utils
+import traceback
+import numpy as np
+import tensorflow as tf
+import models_mnist as models
+import datetime
+import my_utils
+from functools import partial
+# from matplotlib import pyplot as plt
+# from sklearn.manifold import TSNE
+# tsne = TSNE(n_components=2)
+""" param """
+epoch = 40
+batch_size = 100
+lr = 2e-4
+beta1 = 0.5
+z_dim = 128
+n_critic = 1 #
+n_generator = 1
+gan_type="cat-gan"
+dir="results/"+gan_type+"-"+datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+
+''' data '''
+# data_pool = my_utils.getFullMNISTDatapool(batch_size, shift=False) #range 0 ~ 1
+data_pool = my_utils.getFullFashion_MNISTDatapool(batch_size, shift=False)
+X,Y = my_utils.loadFullFashion_MNSIT(shift=False)
+# X, Y = my_utils.load_data('mnist')
+X = np.reshape(X, [70000,28,28,1])
+num_data = 70000
+
+# test_data = [[], [], [], [], [], [], [], [], [], []]
+# colors =  ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'purple', 'pink', 'brown']
+# plt.ion() # enables interactive mode
+# for i, j in zip(X, Y):
+#     if len(test_data[j]) < 100:
+#         test_data[j].append(i)
+# test_data_list = test_data[0]
+# for i in range(1,10):
+#     test_data_list = np.concatenate((test_data_list, test_data[i]))
+
+""" graphs """
+
+generator = models.generator
+encoder = partial(models.cnn_discriminator, name='encoder', out_dim = 10)
+optimizer = tf.train.AdamOptimizer
+
+# inputs
+real = tf.placeholder(tf.float32, shape=[None, 28, 28, 1])
+random_z = tf.random_normal(shape=(batch_size, z_dim),
+                       mean=0, stddev=1, dtype=tf.float32)
+
+#marginal entropy
+def mar_entropy(y):
+    y1 = tf.reduce_mean(y,axis=0)
+    y2=tf.reduce_sum(-y1*tf.log(y1))
+    return y2
+
+#conditional entropy
+def cond_entropy(y):
+    y1=-y*tf.log(y)
+    y2 = tf.reduce_sum(y1)/batch_size
+    return y2
+
+fake = generator(random_z, reuse=False)
+print('fake shape ',fake.shape)
+r_mean = encoder(real, reuse=False)
+f_mean = encoder(fake)
+
+r_p = tf.nn.softmax(r_mean)
+f_p = tf.nn.softmax(f_mean)
+
+
+weight = tf.placeholder(tf.float32, shape=[])
+init_weight = 1.
+
+#discriminator loss
+d_loss = -1 * (mar_entropy(r_p) - cond_entropy(r_p) + weight*cond_entropy(f_p))  # Equation (7) upper
+
+#generator loss
+g_loss = -mar_entropy(f_p) + cond_entropy(f_p)  # Equation (7) lower
+
+
+# trainable variables for each network
+T_vars = tf.trainable_variables()
+d_var = [var for var in T_vars if var.name.startswith('encoder')]
+g_var = [var for var in T_vars if var.name.startswith('generator')]
+
+
+# optimize
+global_step = tf.Variable(0, name='global_step',trainable=False)
+d_step = optimizer(learning_rate=lr, beta1=beta1).minimize(d_loss, var_list=d_var, global_step=global_step)
+g_step = optimizer(learning_rate=lr, beta1=beta1).minimize(g_loss, var_list=g_var)
+
+
+d_reg_loss = d_loss + 0.001 * tf.add_n([ tf.nn.l2_loss(v) for v in d_var])
+d_reg_step = optimizer(learning_rate=lr, beta1=beta1).minimize(d_reg_loss, var_list=d_var)
+
+""" train """
+''' init '''
+# session
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=1)
+sess = tf.InteractiveSession(config=tf.ConfigProto(gpu_options=gpu_options))
+
+# saver
+saver = tf.train.Saver(max_to_keep=5)
+# summary writer
+# Send summary statistics to TensorBoard
+tf.summary.scalar('d_loss', d_loss)
+tf.summary.scalar('g_loss', g_loss)
+images_from_g = generator(random_z, training=False)
+tf.summary.image('Generator_image', images_from_g, 12)
+
+#predict
+embed_mean = encoder(real)
+y = tf.nn.softmax(embed_mean)
+predicts = tf.argmax(y,axis=1)
+
+merged = tf.summary.merge_all()
+logdir = dir+"/tensorboard"
+writer = tf.summary.FileWriter(logdir, sess.graph)
+print('Tensorboard dir: '+logdir)
+
+''' initialization '''
+sess.run(tf.global_variables_initializer())
+
+''' train '''
+batch_epoch = len(data_pool) // (batch_size * n_critic)
+max_it = epoch * batch_epoch
+
+def sample_once(it):
+    rows = 10
+    columns = 10
+    feed = {random_z: np.random.normal(size=[rows * columns, z_dim])}
+    list_of_generators = [images_from_g]  # used for sampling images
+    list_of_names = ['it%d-g.jpg' % it]
+    save_dir = dir + "/sample_imgs"
+    my_utils.sample_and_save(sess=sess, list_of_generators=list_of_generators, feed_dict=feed,
+                             list_of_names=list_of_names, save_dir=save_dir)
+
+def cluster_acc(Y_pred, Y):
+  from sklearn.utils.linear_assignment_ import linear_assignment
+  assert Y_pred.size == Y.size
+  D = max(Y_pred.max(), Y.max())+1
+  w = np.zeros((D,D), dtype=np.int64)
+  for i in range(Y_pred.size):
+    w[Y_pred[i], Y[i]] += 1
+  ind = linear_assignment(w.max() - w)
+  return sum([w[i,j] for i,j in ind])*1.0/Y_pred.size, w
+
+def training(max_it, it_offset):
+    print("Max iteration: " + str(max_it))
+
+    for it in range(it_offset, it_offset + max_it):
+        real_ipt, y = data_pool.batch(['img', 'label'])
+
+        if it//batch_epoch >25:
+            #print('stop G')
+            _ = sess.run([d_reg_step], feed_dict={real: real_ipt, weight: init_weight})
+
+        else:
+            if it%(10*batch_epoch)==0 and it != 0:
+                global init_weight
+                init_weight = init_weight*0.9
+                print('weight', init_weight)
+            _, _ = sess.run([d_step, g_step], feed_dict={real: real_ipt, weight: init_weight})
+
+
+
+
+        if it%10 == 0 :
+
+            summary = sess.run(merged, feed_dict={real: real_ipt,
+                                                   weight: init_weight
+                                                  })
+            writer.add_summary(summary, it)
+        #
+        if it%(batch_epoch) == 0:
+            predict_y = sess.run(predicts, feed_dict={real: X[:2000]})
+            # predict_y_2 = sess.run(predicts, feed_dict={real: X[35000:]})
+            acc = cluster_acc(predict_y, Y[:2000])
+            print('full-acc-EPOCH-%d' % (it // (batch_epoch)), acc[0])
+            dist = [0] * 10
+
+            t_dist = [0] * 10
+            for py, y_ in zip(predict_y, Y[:2000]):
+               dist[py] += 1
+               t_dist[y_] += 1
+            print('true dist: ', np.array(t_dist) / float(len(predict_y)))
+            print('pred dist: ', np.array(dist) / float(len(predict_y)))
+
+
+
+    var = raw_input("Continue training for %d iterations?" % max_it)
+    if var.lower() == 'y':
+        # sample_once(it_offset + max_it)
+        # print("Save sample images")
+        training(max_it, it_offset + max_it)
+
+total_it = 0
+try:
+    # en_saver = tf.train.Saver(var_list=d_var)
+    #en_saver.restore(sess, 'results/ae-dc-20180426-133140/checkpoint/model.ckpt')  # ep5
+    #en_saver.restore(sess, 'results/ae-dc-20180426-134158/checkpoint/model.ckpt') # ep5
+    #en_saver.restore(sess, 'results/ae-dc-20180426-143609/checkpoint/model.ckpt') # ep55
+    #en_saver.restore(sess, 'results/ae-dc-20180426-145357/checkpoint/model.ckpt') # ep90
+    training(max_it,0)
+    total_it = sess.run(global_step)
+    print("Total iterations: "+str(total_it))
+except Exception, e:
+    traceback.print_exc()
+finally:
+    save_path = saver.save(sess, dir+"/checkpoint/model.ckpt")
+    print("Model saved in path: %s" % save_path)
+    print(" [*] Close main session!")
+    sess.close()
---
+++ models_mnist.py
@@ -0,0 +1,426 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import ops
+import tensorflow as tf
+from functools import partial
+import tensorflow.contrib.slim as slim
+
+fc = partial(ops.flatten_fully_connected, activation_fn=None, weights_initializer=tf.random_normal_initializer(stddev=0.02))
+relu = tf.nn.relu
+lrelu = partial(ops.leak_relu, leak=0.2)
+conv = partial(slim.conv2d, activation_fn=None)
+dconv = partial(slim.conv2d_transpose, activation_fn=None)
+batch_norm = partial(slim.batch_norm, decay=0.9, scale=True, epsilon=1e-5, updates_collections=None)
+lrelu_2 = partial(ops.leak_relu, leak=0.1)
+
+def encoder(x, reuse=True, name="encoder", z_dim=10):
+    fc_relu = partial(fc, activation_fn=relu)
+    with tf.variable_scope(name, reuse=reuse):
+        y = fc_relu(x, 500, scope='layer1')
+        y = fc_relu(y, 500, scope="layer2")
+        y = fc_relu(y, 2000, scope="layer3")
+        z_mean = fc(y,z_dim, scope='mean_layer')
+        z_log_var = fc(y, z_dim, scope='log_var_layer')
+        return z_mean, z_log_var
+
+def dc_encoder(img, dim=64, reuse=True, training=True, name= 'encoder', z_dim=10):
+    bn = partial(batch_norm, is_training=training)
+    conv_bn_lrelu = partial(conv, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
+    fc_bn_lrelu = partial(fc, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
+
+    with tf.variable_scope(name, reuse=reuse):
+        y = lrelu(conv(img, 1, 5, 2))
+        y = conv_bn_lrelu(y, dim, 5, 2)
+        y = fc_bn_lrelu(y, 1024)
+        z_mean = fc(y, z_dim)
+        z_log_var = fc(y, z_dim)
+        return z_mean, z_log_var
+
+def sampleing(z_mean, z_log_var):
+    eps = tf.random_normal(shape=tf.shape(z_log_var),
+                           mean=0, stddev=1, dtype=tf.float32)
+    z = z_mean + tf.exp(z_log_var/2) * eps
+    return z
+
+def decoder(z, reuse=True, name="decoder"):
+    fc_relu = partial(fc, activation_fn=relu)
+    fc_sigmoid = partial(fc, activation_fn=tf.nn.sigmoid)
+    with tf.variable_scope(name, reuse=reuse):
+        y = fc_relu(z, 2000, scope='layer1')
+        y = fc_relu(y, 500, scope='layer2')
+        y = fc_relu(y, 500,scope='layer3')
+        x = fc_sigmoid(y,784, scope='output_layer')
+        x = tf.reshape(x, [-1, 28, 28, 1])
+        return x
+
+def dc_decoder(z, dim=64, reuse=True, training=True, name="decoder"):
+        bn = partial(batch_norm, is_training=training)
+        dconv_bn_relu = partial(dconv, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+        fc_bn_relu = partial(fc, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+
+        with tf.variable_scope(name, reuse=reuse):
+            y = fc_bn_relu(z, 1024)
+            y = fc_bn_relu(y, 7 * 7 * dim * 2)
+            y = tf.reshape(y, [-1, 7, 7, dim * 2])
+            y = dconv_bn_relu(y, dim * 2, 5, 2)
+            img = tf.sigmoid(dconv(y, 1, 5, 2))
+            return img
+
+def decoder2(z, reuse=True, name="decoder"):
+    fc_relu = partial(fc, activation_fn=relu)
+    fc_tahn = partial(fc, activation_fn=tf.nn.tanh)
+    with tf.variable_scope(name, reuse=reuse):
+        y = fc_relu(z, 2000, scope='layer1')
+        y = fc_relu(y, 500, scope='layer2')
+        y = fc_relu(y, 500,scope='layer3')
+        x = fc_tahn(y,784, scope='output_layer')
+        x = tf.reshape(x, [-1, 28, 28, 1])
+        return x
+
+def discriminator_for_latent(x, reuse=True, name = "discriminator"):
+    fc_lrelu = partial(fc, normalizer_fn=None, activation_fn=lrelu)
+    with tf.variable_scope(name, reuse=reuse):
+        y = fc_lrelu(x, 512)
+        y = fc_lrelu(y, 1024)
+        return fc(y, 1)
+
+def multi_c_discriminator(x, out_c=11,reuse=True, name = "discriminator"):
+    fc_lrelu = partial(fc, normalizer_fn=None, activation_fn=lrelu)
+    with tf.variable_scope(name, reuse=reuse):
+        y = tf.reshape(x, [-1, 784])
+        y = fc_lrelu(y, 1024)
+        y = fc_lrelu(y, 1024)
+        return fc(y, out_c)
+
+# selective sampling
+def ss_generator(z, reuse=True, name="generator", training=True):
+    bn = partial(batch_norm, is_training=training)
+    # fc_relu = partial(fc, normalizer_fn=None, activation_fn=relu)
+    fc_bn_relu = partial(fc, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+    with tf.variable_scope(name, reuse=reuse):
+        y = fc_bn_relu(z, 1024)
+        y = tf.sigmoid(fc(y, 784))
+        y = tf.reshape(y, [-1, 28, 28, 1])
+        return y
+
+def ss_discriminator(x, reuse=True, name = "discriminator"):
+    fc_lrelu = partial(fc, normalizer_fn=None, activation_fn=lrelu)
+    with tf.variable_scope(name, reuse=reuse):
+        y =  tf.reshape(x, [-1, 784])
+        y = fc_lrelu(y, 1024)
+        return fc(y, 1)
+
+def ss_generator_m(z, heads=10,reuse=True, name = "generator", training = True):
+    bn = partial(batch_norm, is_training=training)
+    fc_bn_relu = partial(fc, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+    with tf.variable_scope(name, reuse=reuse):
+        y = fc_bn_relu(z, 1024)
+        y = fc_bn_relu(y, 1024)
+        img_sets = []
+        for _ in range(heads):
+            out = tf.sigmoid(fc(y, 784))
+            out = tf.reshape(out, [-1, 28, 28, 1])
+            img_sets.append(out)
+        return img_sets
+
+def generator_m(z, heads=10, dim=64, reuse=True, training=True, name="generator"):
+    bn = partial(batch_norm, is_training=training)
+    dconv_bn_relu = partial(dconv, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+    fc_bn_relu = partial(fc, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+
+    with tf.variable_scope(name, reuse=reuse):
+        y = fc_bn_relu(z, 1024)
+        y = fc_bn_relu(y, 7 * 7 * dim * 2)
+        y = tf.reshape(y, [-1, 7, 7, dim * 2])
+        y = dconv_bn_relu(y, dim * 2, 5, 2)
+        img_sets = []
+        for _ in range(heads):
+            img_sets.append(tf.sigmoid(dconv(y, 1, 5, 2)))
+
+        return img_sets
+
+def generator(z, dim=64, reuse=True, training=True, name="generator"):
+    bn = partial(batch_norm, is_training=training)
+    dconv_bn_relu = partial(dconv, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+    fc_bn_relu = partial(fc, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+
+    with tf.variable_scope(name, reuse=reuse):
+        y = fc_bn_relu(z, 1024)
+        y = fc_bn_relu(y, 7 * 7 * dim * 2)
+        y = tf.reshape(y, [-1, 7, 7, dim * 2])
+        y = dconv_bn_relu(y, dim * 2, 5, 2)
+        y = tf.sigmoid(dconv(y, 1, 5, 2))
+        return y
+
+def discriminator(img, dim=64, reuse=True, training=True, name= 'discriminator'):
+    bn = partial(batch_norm, is_training=training)
+    conv_bn_lrelu = partial(conv, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
+    fc_bn_lrelu = partial(fc, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
+
+    with tf.variable_scope(name, reuse=reuse):
+        y = lrelu(conv(img, 1, 5, 2))
+        y = conv_bn_lrelu(y, dim, 5, 2)
+        y = fc_bn_lrelu(y, 1024)
+        logit = fc(y, 1)
+        return logit
+
+from ops2 import conv2d,bn,linear,deconv2d
+def discriminator2( x, training=True, reuse=True , name="discriminator"):
+        # Network Architecture is exactly same as in infoGAN (https://arxiv.org/abs/1606.03657)
+        # Architecture : (64)4c2s-(128)4c2s_BL-FC1024_BL-FC1_S
+    with tf.variable_scope(name, reuse=reuse):
+        net = lrelu(conv2d(x, 64, 4, 4, 2, 2, name='d_conv1'))
+        net = lrelu(bn(conv2d(net, 128, 4, 4, 2, 2, name='d_conv2'), is_training=training, scope='d_bn2'))
+        # net = tf.reshape(net, [batch_size, -1])
+        # net = lrelu(bn(linear(net, 1024, scope='d_fc3'), is_training=training, scope='d_bn3'))
+        net = fc(net,1024)
+        net = lrelu(bn(net, is_training=training, scope='d_bn3'))
+        out_logit = linear(net, 1, scope='d_fc4')
+        # out = tf.nn.sigmoid(out_logit)
+
+        return out_logit
+
+def generator2(z, training=True, reuse=True, name = "generator"):
+        # Network Architecture is exactly same as in infoGAN (https://arxiv.org/abs/1606.03657)
+        # Architecture : FC1024_BR-FC7x7x128_BR-(64)4dc2s_BR-(1)4dc2s_S
+    with tf.variable_scope(name, reuse=reuse):
+        net = tf.nn.relu(bn(linear(z, 1024, scope='g_fc1'), is_training=training, scope='g_bn1'))
+        net = tf.nn.relu(bn(linear(net, 128 * 7 * 7, scope='g_fc2'), is_training=training, scope='g_bn2'))
+        net = tf.reshape(net, [64, 7, 7, 128])
+        net = tf.nn.relu(
+                bn(deconv2d(net, [64, 14, 14, 64], 4, 4, 2, 2, name='g_dc3'), is_training=training,
+                   scope='g_bn3'))
+
+        out = tf.nn.sigmoid(deconv2d(net, [64, 28, 28, 1], 4, 4, 2, 2, name='g_dc4'))
+
+        return out
+
+def generator2_mimic(z, training=True, reuse=True, name="generator"):
+    bn = partial(batch_norm, is_training=training)
+    dconv_bn_relu = partial(dconv, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+    fc_bn_relu = partial(fc, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+        # Network Architecture is exactly same as in infoGAN (https://arxiv.org/abs/1606.03657)
+        # Architecture : FC1024_BR-FC7x7x128_BR-(64)4dc2s_BR-(1)4dc2s_S
+    with tf.variable_scope(name, reuse=reuse):
+        net = fc_bn_relu(z, 1024)
+        net = fc_bn_relu(net, 128 * 7 * 7)
+        net = tf.reshape(net, [-1, 7, 7, 128])
+        net = dconv_bn_relu(net, 64, 4, 2)
+        net = dconv_bn_relu(net, 1, 4, 2)
+        out = tf.nn.sigmoid(net)
+        # net = tf.nn.relu(
+        #         bn(deconv2d(net, [64, 14, 14, 64], 4, 4, 2, 2, name='g_dc3'), is_training=training,
+        #            scope='g_bn3'))
+        #
+        # out = tf.nn.sigmoid(deconv2d(net, [64, 28, 28, 1], 4, 4, 2, 2, name='g_dc4'))
+
+        return out
+
+def generator_m2(z,heads=10, training=True, reuse=True, name="generator"):
+
+        # Network Architecture is exactly same as in infoGAN (https://arxiv.org/abs/1606.03657)
+        # Architecture : FC1024_BR-FC7x7x128_BR-(64)4dc2s_BR-(1)4dc2s_S
+    with tf.variable_scope(name, reuse=reuse):
+        net = tf.nn.relu(bn(linear(z, 1024, scope='g_fc1'), is_training=training, scope='g_bn1'))
+        net = tf.nn.relu(bn(linear(net, 128 * 7 * 7, scope='g_fc2'), is_training=training, scope='g_bn2'))
+        net = tf.reshape(net, [64, 7, 7, 128])
+        net = tf.nn.relu(
+                bn(deconv2d(net, [64, 14, 14, 64], 4, 4, 2, 2, name='g_dc3'), is_training=training,
+                   scope='g_bn3'))
+
+        img_sets = []
+        for i in range(heads):
+            out = tf.nn.sigmoid(deconv2d(net, [64, 28, 28, 1], 4, 4, 2, 2, name='g_dc4_h'+str(i)))
+            img_sets.append(out)
+
+        return img_sets
+
+
+
+
+def cnn_classifier(x,keep_prob, out_dim=10,name="classifier", reuse=True):
+    with tf.variable_scope(name, reuse=reuse):
+        # Convolutional Layer #1
+        conv1 = tf.layers.conv2d(
+            inputs=x,
+            filters=32,
+            kernel_size=[5, 5],
+            padding="same",
+            activation=tf.nn.relu)
+
+        # Pooling Layer #1
+        pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+        # Convolutional Layer #2 and Pooling Layer #2
+        conv2 = tf.layers.conv2d(
+            inputs=pool1,
+            filters=64,
+            kernel_size=[5, 5],
+            padding="same",
+            activation=tf.nn.relu)
+        pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+        # Dense Layer
+        pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+        dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+        dropout = tf.layers.dropout(
+            inputs=dense, rate=1-keep_prob)
+
+        # Logits Layer
+        logits = tf.layers.dense(inputs=dropout, units=out_dim)
+        # y = tf.nn.softmax(logits)
+        return logits
+
+def generator(z, dim=64, reuse=True, training=True, name="generator"):
+    bn = partial(batch_norm, is_training=training)
+    dconv_bn_relu = partial(dconv, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+    fc_bn_relu = partial(fc, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+
+    with tf.variable_scope(name, reuse=reuse):
+        y = fc_bn_relu(z, 1024)
+        y = fc_bn_relu(y, 7 * 7 * dim * 2)
+        y = tf.reshape(y, [-1, 7, 7, dim * 2])
+        y = dconv_bn_relu(y, dim * 2, 5, 2)
+        img = tf.sigmoid(dconv(y, 1, 5, 2))
+        return img
+
+def cat_generator(z,reuse=True, name = "generator", training = True):
+    bn = partial(batch_norm, is_training=training)
+    fc_bn_lrelu = partial(fc, normalizer_fn=bn, activation_fn=lrelu_2, biases_initializer=None)
+    fc_bn_lrelu_2 = partial(fc, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
+    with tf.variable_scope(name, reuse=reuse):
+        y = fc_bn_lrelu(z, 500)
+        y = fc_bn_lrelu(y, 500)
+        y = fc_bn_lrelu_2(y, 1000)
+
+        y = tf.sigmoid(fc(y, 784))
+        y = tf.reshape(y, [-1, 28, 28, 1])
+            # out_put_sets.append(y_1)
+        return y
+
+def cat_discriminator(z, out_dim=10, reuse=True, name = "discriminator", training = True, stddev=0.3):
+    bn = partial(batch_norm, is_training=training)
+    fc_bn_lrelu = partial(fc, normalizer_fn=bn, activation_fn=lrelu_2, biases_initializer=None)
+    # fc_bn_lrelu_2 = partial(fc, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
+    with tf.variable_scope(name, reuse=reuse):
+        y = z + tf.random_normal(shape=tf.shape(z),mean=0, stddev=stddev, dtype=tf.float32)
+        y = fc_bn_lrelu(y, 1000)
+        y = y + tf.random_normal(shape=tf.shape(y),mean=0, stddev=stddev, dtype=tf.float32)
+        y = fc_bn_lrelu(y, 500)
+        y = y + tf.random_normal(shape=tf.shape(y), mean=0, stddev=stddev, dtype=tf.float32)
+        y = fc_bn_lrelu(y, 250)
+        y = y + tf.random_normal(shape=tf.shape(y), mean=0, stddev=stddev, dtype=tf.float32)
+        y = fc_bn_lrelu(y, 250)
+        y = y + tf.random_normal(shape=tf.shape(y), mean=0, stddev=stddev, dtype=tf.float32)
+        y = fc_bn_lrelu(y, 250)
+        y = y + tf.random_normal(shape=tf.shape(y), mean=0, stddev=stddev, dtype=tf.float32)
+        logits = fc(y, out_dim)
+        # y_out = tf.nn.softmax(logits)
+        return logits
+
+def cat_discriminator2(z, out_dim=10, reuse=True, name = "discriminator", training = True, stddev=0.3):
+    bn = partial(batch_norm, is_training=training)
+    fc_bn_lrelu = partial(fc, normalizer_fn=bn, activation_fn=lrelu_2, biases_initializer=None)
+    # fc_bn_lrelu_2 = partial(fc, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
+    with tf.variable_scope(name, reuse=reuse):
+        # y = z + tf.random_normal(shape=tf.shape(z),mean=0, stddev=stddev, dtype=tf.float32)
+        y = fc_bn_lrelu(z, 1000)
+        # y = y + tf.random_normal(shape=tf.shape(y),mean=0, stddev=stddev, dtype=tf.float32)
+        y = fc_bn_lrelu(y, 500)
+        # y = y + tf.random_normal(shape=tf.shape(y), mean=0, stddev=stddev, dtype=tf.float32)
+        y = fc_bn_lrelu(y, 250)
+        # y = y + tf.random_normal(shape=tf.shape(y), mean=0, stddev=stddev, dtype=tf.float32)
+        y = fc_bn_lrelu(y, 250)
+        # y = y + tf.random_normal(shape=tf.shape(y), mean=0, stddev=stddev, dtype=tf.float32)
+        y = fc_bn_lrelu(y, 250)
+        # y = y + tf.random_normal(shape=tf.shape(y), mean=0, stddev=stddev, dtype=tf.float32)
+        logits = fc(y, out_dim)
+        # y_out = tf.nn.softmax(logits)
+        return logits
+
+def cluster_layer(z, out_c=10,reuse=True, name = "cluster"):
+    with tf.variable_scope(name, reuse=reuse):
+        y = fc(z, out_c)
+        return tf.nn.softmax(y)
+
+def entropy(q):
+    return tf.reduce_sum(-q*tf.log(q), axis=1)
+
+def allconvnet_mnist(x, out_dim=10,name="classifier", training=True, reuse=True):
+    # w_init = tf.variance_scaling_initializer(scale=2., mode='fan_in')
+    bn = partial(batch_norm, is_training=training)
+    # conv_relu = partial(conv, activation_fn=relu)#, weights_initializer=w_init)
+    conv_bn_relu = partial(conv, activation_fn=relu, normalizer_fn=bn,biases_initializer=None)#, weights_initializer=w_init)
+    with tf.variable_scope(name, reuse=reuse):
+        # Convolutional Layer #1
+        y = conv_bn_relu(x, 64, 3, 1)
+        y = conv_bn_relu(y, 64, 3, 1)
+        y = conv_bn_relu(y, 64, 3, 2)
+        y = conv_bn_relu(y, 128, 3, 1)
+        y = conv_bn_relu(y, 128, 3, 1)
+        y = conv_bn_relu(y, 128, 3, 2)
+        y = conv_bn_relu(y, out_dim, 1, 1)
+        # print(y.shape)
+
+        y = tf.reduce_mean(y, [1,2])
+        y = bn(y)
+        #restraint layer
+        y_max = tf.reduce_max(y, axis=1, keep_dims=True)
+        y = tf.exp(y - y_max)
+        y = y/tf.norm(y, ord=2, axis=1, keep_dims=True)
+        return y
+
+def cnn_discriminator(z, out_dim=10, reuse=True, name = "discriminator" , training=True):
+    lrelu_1 = partial(ops.leak_relu, leak=0.1)
+    conv_lrelu = partial(conv, activation_fn=lrelu_1)
+    fc_lrelu = partial(fc, activation_fn=lrelu_1)
+    with tf.variable_scope(name, reuse=reuse):
+        y = conv_lrelu(z, 32, 5, 1)
+        y = tf.layers.max_pooling2d(inputs=y, pool_size=[3, 3], strides=2)
+        y = conv_lrelu(y, 64, 3, 1)
+        y = conv_lrelu(y, 64, 3, 1)
+        y = tf.layers.max_pooling2d(inputs=y, pool_size=[3, 3], strides=2)
+        y = conv_lrelu(y, 128, 3, 1)
+        y = conv_lrelu(y, 10, 1, 1)
+        y = fc_lrelu(y, 128)
+        y = fc(y,out_dim)
+        return y
+
+def cnn_discriminator_cifar(z, out_dim=10, reuse=True, name = "discriminator" , training=True):
+    lrelu_1 = partial(ops.leak_relu, leak=0.1)
+    conv_lrelu = partial(conv, activation_fn=lrelu_1)
+    fc_lrelu = partial(fc, activation_fn=lrelu_1)
+    with tf.variable_scope(name, reuse=reuse):
+        y = conv_lrelu(z, 96, 3, 1)
+        y = conv_lrelu(y, 96, 3, 1)
+        y = conv_lrelu(y, 96, 3, 1)
+        y = tf.layers.max_pooling2d(inputs=y, pool_size=[2, 2], strides=2)
+        y = conv_lrelu(y, 192, 3, 1)
+        y = conv_lrelu(y, 192, 3, 1)
+        y = conv_lrelu(y, 192, 3, 1)
+        y = tf.layers.max_pooling2d(inputs=y, pool_size=[3, 3], strides=2)
+        y = conv_lrelu(y, 192, 3, 1)
+        y = conv_lrelu(y, 192, 1, 1)
+        y = conv_lrelu(y, 10, 1, 1)
+        y = tf.reduce_mean(y, [1, 2])
+        y = fc(y,out_dim)
+        return y
+
+def cnn_generator(z, reuse = True, name = "generator", training=True):
+    lrelu_1 = partial(ops.leak_relu, leak=0.1)
+    dconv_lrelu = partial(dconv,  activation_fn=lrelu_1)
+    fc_lrelu = partial(fc, activation_fn=lrelu_1)
+    conv_lrelu = partial(conv, activation_fn=lrelu_1)
+    with tf.variable_scope(name, reuse=reuse):
+        y = fc_lrelu(z, 7*7*96)
+        y = tf.reshape(y,shape=[-1,7,7,96])
+        y = dconv_lrelu(y, 1, 5, 2)
+        y = conv_lrelu(y, 64, 5, 1)
+        y = dconv_lrelu(y, 1, 5, 2)
+        y = conv_lrelu(y, 64, 5, 1)
+        y = conv_lrelu(y, 1, 5, 1)
+
+        # out_put_sets.append(y_1)
+        return y
